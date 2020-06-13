fe51820ccb9dfe53d9ae2284e9b1bdf0e1d5d106
Deepak Vishwakarma <29451111+deebeast@users.noreply.github.com>
2020-06-14 01:55:49 +0530

    Init for the folder

---
+++ BehaviorAndState/com/deebeast/behaviorandstate/QuickStart.java
@@ -0,0 +1,11 @@
+/**
+ * QuickStart
+ */
+package com.deebeast.behaviorandstate;
+
+public class QuickStart {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
